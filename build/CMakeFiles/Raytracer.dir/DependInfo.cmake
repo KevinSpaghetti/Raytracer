95147e0c34cd921c502b2f1ffa085321165493b9
Kevin Della Schiava <dellaschiavak@gmail.com>
2020-09-09 23:04:11 +0200

    Update gitignore

---
+++ build/CMakeFiles/Raytracer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Raytracer/Geom/AABB.cpp" "E:/Raytracer/build/CMakeFiles/Raytracer.dir/Geom/AABB.cpp.obj"
+  "E:/Raytracer/Geom/Ray.cpp" "E:/Raytracer/build/CMakeFiles/Raytracer.dir/Geom/Ray.cpp.obj"
+  "E:/Raytracer/Geom/Rect.cpp" "E:/Raytracer/build/CMakeFiles/Raytracer.dir/Geom/Rect.cpp.obj"
+  "E:/Raytracer/Geom/Sphere.cpp" "E:/Raytracer/build/CMakeFiles/Raytracer.dir/Geom/Sphere.cpp.obj"
+  "E:/Raytracer/ImageTexture.cpp" "E:/Raytracer/build/CMakeFiles/Raytracer.dir/ImageTexture.cpp.obj"
+  "E:/Raytracer/main.cpp" "E:/Raytracer/build/CMakeFiles/Raytracer.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
